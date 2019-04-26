b8c39371fac9abe0b7819f0928d3751c573aa06c
davidkartuzinski <david@kaidawei.me>
2019-04-26 10:06:33 +0200

    video 8 gatsby images and readme

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,8 +1,16 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
 const path = require(`path`)
 
 const makeRequest = (graphql, request) =>
   new Promise((resolve, reject) => {
-    // Query for nodes to use in creating pages.
+    // Query for article nodes to use in creating pages.
     resolve(
       graphql(request).then(result => {
         if (result.errors) {
@@ -45,6 +53,32 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     })
   })
 
-  // Query for articles nodes to use in creating pages.
-  return getArticles
+  const getAuthors = makeRequest(
+    graphql,
+    `
+    {
+      allStrapiUser {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+    `
+  ).then(result => {
+    // Create pages for each user.
+    result.data.allStrapiUser.edges.forEach(({ node }) => {
+      createPage({
+        path: `/authors/${node.id}`,
+        component: path.resolve(`src/templates/author.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  })
+
+  // Queries for articles and authors nodes to use in creating pages.
+  return Promise.all([getArticles, getAuthors])
 }
